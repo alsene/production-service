7032dee950bfe635e939371734b70923a5b16a24
asene <devs-canada@open-it.sn>
2026-08-12 17:03:55 -0400

    ajout de la config Prod

--- src/main/java/com/production/api/configuration/WebConfig.java
+++ src/main/java/com/production/api/configuration/WebConfig.java
@@ -1,8 +1,6 @@
 package com.production.api.configuration;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,16 +9,10 @@ import java.util.Arrays;
 
 @Slf4j
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-//    private final String allowedOrigins="http://localhost:4200, https://sentechno-dev.com, https://www.sentechno-dev.com, https://www.sentechno-dev.com/login, https://api.sentechno-dev.com"; // Valeurs par défaut
-    @Value("${app.cors.allowed-origins}")
-    private String allowedOrigins;
 
-    public WebConfig(String allowedOrigins) {
-        this.allowedOrigins = allowedOrigins;
-    }
+    private final String allowedOrigins="http://localhost:4200,https://sentechno-dev.com,https://www.sentechno-dev.com";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -28,8 +20,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .map(String::trim)
                 .toArray(String[]::new);
 
-        registry.addMapping("/api/**") // Applique à tous les endpoints
-                .allowedOrigins(origins) // URL de votre app Angular //Access-Control-Allow-Origin: https://example.com, http://localhost:4200, Header set Access-Control-Allow-Origin 'origin-list'
+        registry.addMapping("/api/**")
+                .allowedOrigins(origins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
